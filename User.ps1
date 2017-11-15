6ca7ffb0ef86894723f0dd8959dae5bf2824896d
Leigh London <leigh.00.london@gmail.com>
2017-11-15 13:58:33 +1100

    Changes to the SID call.
    Adding in a try/catch block, changing the parameter configuration, and a
    bit better error handling.

---
+++ User.ps1
@@ -0,0 +1,18 @@
+function ConvertTo-SID {
+    [CmdletBinding()]
+    param(
+        [parameter(mandatory=$TRUE)]
+        [String]
+        $username
+    )
+    process {
+        try {
+            $u = New-Object System.Security.Principal.NTAccount($username)
+            $sid = $u.Translate([System.Security.Principal.SecurityIdentifier])
+            $sid.Value
+        }
+        catch {
+            Write-Warning $_
+        }
+    }
+}
