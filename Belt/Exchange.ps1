462596f04244ed1c108de2ee3233d010e624bfac
Leigh London <leigh.00.london@gmail.com>
2017-11-17 17:50:51 +1100

    exchange: logical reordering

--- Belt/Exchange.ps1
+++ Belt/Exchange.ps1
@@ -1,13 +1,3 @@
-function Test-ExchangeConnection {
-    # test for a valid connection to exchange by looking for a cmdlet.
-    $exists = [bool](Get-Command -Name Check-MailboxQuotas -ErrorAction SilentlyContinue)
-    if ($exists -eq $FALSE) {
-        Write-Warning "Not connected to Exchange"
-        # TODO: better error handling
-        Exit
-    }
-}
-
 <#
 .SYNOPSIS
 List the forwards active on a mailbox.
@@ -25,5 +15,15 @@ function Get-MailboxForwards {
     begin { Test-ExchangeConnection }
     process {
         Get-Mailbox -Mailbox $mailbox | Select-Object -Property ForwardingAddress,ForwardingSmtpAddress
+    }
+}
+
+function Test-ExchangeConnection {
+    # test for a valid connection to exchange by looking for a cmdlet.
+    $exists = [bool](Get-Command -Name Check-MailboxQuotas -ErrorAction SilentlyContinue)
+    if ($exists -eq $FALSE) {
+        Write-Warning "Not connected to Exchange"
+        # TODO: better error handling
+        Exit
     }
 }
