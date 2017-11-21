f00add1c6bc1fb1829599c78927108019a3899fa
Leigh London <leigh.00.london@gmail.com>
2017-11-22 10:04:59 +1100

    mailbox: decoding the mailbox type
    Decoding the exchRemoteRecipientType attribute into a string.
    This attribute is typically part of an Active Directory user.

---
+++ Belt/Mailbox.ps1
@@ -0,0 +1,23 @@
+
+function Resolve-MailboxType {
+    param([Int64]$mbox)
+    $types = @{
+        1='ProvisionedCloudMailbox'
+        2='ProvisionedCloudArchive'
+        4='MigratedMailbox'
+        8='DeprovisionedMailbox'
+        16='DeprovisionedArchive'
+        32='RoomMailbox'
+        64='EquipmentMailbox'
+        96='SharedMailbox'
+    }
+    # use a dynamic type as temporary storage
+    $result = [System.Collections.ArrayList]@()
+    $types.Keys |
+        # bitwise operations to decode the type.
+        where { $mbox -band $_ } |
+        # swallow the return value
+        foreach { $_ = $result.Add($types.Get_Item($_)) }
+    # returns a human-readable single string
+    $result -Join ', '
+}
