592ede0d7a708beb0c317c971290a311133dc05e
Leigh London <leigh.00.london@gmail.com>
2017-11-23 19:13:58 +1100

    meta: refactor
    Re-organise based on what it hooks into.

--- Belt/ActiveDirectory.ps1
+++ Belt/ActiveDirectory.ps1
@@ -17,3 +17,47 @@ function Get-GroupMembers {
         throw $_
     }
 }
+
+function Get-MailboxType {
+    param([string]$username)
+    $type = @{
+        'Name' = 'Mailbox Type'
+        'Expression' = { Resolve-MailboxType $_.msExchRemoteRecipientType }
+    }
+    try {
+        Get-ADUser $username -Properties msExchRemoteRecipientType |
+            Select-Object Name, $type
+    } catch {
+        'Not found: ' + $username | Write-Warning
+    }
+}
+
+<#
+.SYNOPSIS
+Resolve the numeric mailbox type to a string.
+
+.DESCRIPTION
+Returns a human-readable string translation of the msExchRemoteRecipientType prop.
+#>
+function Resolve-MailboxType {
+    param([Int64]$mbox)
+    $types = @{
+        1='ProvisionedCloudMailbox'
+        2='ProvisionedCloudArchive'
+        4='MigratedMailbox'
+        8='DeprovisionedMailbox'
+        16='DeprovisionedArchive'
+        32='RoomMailbox'
+        64='EquipmentMailbox'
+        96='SharedMailbox'
+    }
+    # use a dynamic type as temporary storage
+    $result = [System.Collections.ArrayList]@()
+    $types.Keys |
+        # bitwise operations to decode the type
+        where { $mbox -band $_ } |
+        # swallow the return value
+        foreach { $_ = $result.Add($types.Get_Item($_)) }
+    # returns a human-readable single string
+    $result -Join ', '
+}
