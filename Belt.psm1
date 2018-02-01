d84237160fbc1a889ad096d1b8af9e04cfdade5f
Leigh London <leigh.00.london@gmail.com>
2018-02-01 16:37:13 +1100

    meta: lint the module loader

--- Belt.psm1
+++ Belt.psm1
@@ -1,2 +1,2 @@
 # Load all of the scripts under .\Belt\ manually.
-Get-ChildItem -Path $PSScriptRoot\Belt\*.ps1 | foreach { . $_.FullName }
+Get-ChildItem -Path $PSScriptRoot\Belt\*.ps1 | ForEach-Object { . $_.FullName }
