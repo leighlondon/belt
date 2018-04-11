4b0729aaedc93acd392b022ddfff1b4c414c0d65
Leigh London <leigh.00.london@gmail.com>
2018-04-11 17:44:22 +1000

    deploy: set the right version

--- Scripts/Deploy.ps1
+++ Scripts/Deploy.ps1
@@ -2,16 +2,20 @@ if ($env:APPVEYOR_REPO_TAG -ne $true) {
     Write-Information "not deploying, no tag"
     return
 } else {
-    $Manifest = Test-ModuleManifest '.\Belt.psd1'
-    $Version = $Manifest.Version
-    $ModulePath = $env:PSModulePath.Split(';')[0]
     # deploy since it's a tagged release.
+    Write-Information "deploying..."
+    # load in the manifest to get the version number.
+    $Manifest = Test-ModuleManifest '.\Belt.psd1'
+    # calculate the module path for a windows system.
+    $ModulePath = $env:PSModulePath.Split(';')[0]
+    # prepare to install the module manually.
     $ZipParams = @{
         Path = '.\Belt.zip'
-        DestinationPath = "$ModulePath\Belt\$Version"
+        DestinationPath = "$ModulePath\Belt\$($Manifest.Version)"
         Verbose = $true
     }
     Expand-Archive @ZipParams
+    # load and publish
     Import-Module Belt
     Publish-Module -Name 'Belt' -NuGetApiKey $env:DeployKey -Verbose
 }
